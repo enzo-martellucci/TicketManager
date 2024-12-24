1f618dbe79b35d847cae53a37c47a860303fa23e
enzo-martellucci <enzo.martellucciii@gmail.com>
2024-12-24 09:43:50 +0100

    Add basic authentication and navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./assets/**/*.js",
-    "./templates/**/*.html.twig",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./assets/**/*.js",
+        "./templates/**/*.html.twig",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'dark-blue': '#01161E',
+                'blue': '#124559',
+                'light-blue': '#598392',
+                'dark-cream': '#AEC3B0',
+                'light-cream': '#EFF6E0',
+            },
+        },
+    },
+    plugins: [],
 }
