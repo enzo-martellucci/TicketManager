2b69cd2c6d466e84d87f4d99e522de0de62388de
CARPENTIER Simon <simon.carpentier000@etu.univ-littoral.fr>
2024-12-26 00:08:01 +0100

    update add statistics view
    Functionnal statistics view

--- src/Controller/StatisticsController.php
+++ src/Controller/StatisticsController.php
@@ -4,14 +4,20 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\TicketRepository;
 
 class StatisticsController extends AbstractController
 {
     #[Route('/statistics', name: 'statistics_index')]
-    public function index(): Response
+    public function index(TicketRepository $ticketRepository): Response
     {
+        $ticketsByStatus = $ticketRepository->countTicketsByStatus();
+        $ticketsByPriority = $ticketRepository->countTicketsByPriority();
+
         return $this->render('statistics/index.html.twig', [
+            'ticketsByStatus' => json_encode($ticketsByStatus),
+            'ticketsByPriority' => json_encode($ticketsByPriority),
         ]);
     }
 }
